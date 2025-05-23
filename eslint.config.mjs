27488d8d9f65c1802f09eea5764eff0f3120ee2f
bolinc <bolinchan2046@gmail.com>
2025-05-23 21:14:13 +0800

    fix: update configuration files and improve code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
-import antfu from '@antfu/eslint-config'
+import antfu from "@antfu/eslint-config";
 
 export default antfu({
   formatters: true,
-})
+  typescript: true,
+  stylistic: { semi: true, quotes: "double" },
+});
